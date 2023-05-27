4bedf68e378a9c24208b44c6633e812ef4e10002
RioGuglielmelli <rio.guglielmelli@gmail.com>
2023-05-27 12:32:16 -0300

    Created and added UI for tableau. Added all card and background assets. Made changes to UI colour scheme.

--- button.py
+++ button.py
@@ -12,6 +12,8 @@ class Button():
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
 		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
+
+
 	def update(self, screen):
 		if self.image is not None:
 			screen.blit(self.image, self.rect)
--- main.py
+++ main.py
@@ -5,10 +5,18 @@ from button import Button
 pygame.init()
 
 SCREEN = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("Menu")
+pygame.display.set_caption("Solitaire")
+
+red = pygame.Color(255, 0, 0)
+darkgreen = pygame.Color(86, 100, 86)
+green = pygame.Color(166, 178, 162)
+blue = pygame.Color(7, 11, 61)
+cream = pygame.Color(231, 231, 221)
+
+
 
 BG = pygame.image.load("assets/Background.png")
-
+GBG = pygame.image.load("assets/GameBackground.png")
 
 def get_font(size):  # Returns Press-Start-2P in the desired size
     return pygame.font.Font("assets/font.ttf", size)
@@ -18,14 +26,15 @@ def play():
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.fill("black")
+        SCREEN.fill("BLACK")
+        SCREEN.blit(GBG, (0, 0))
 
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(400, 260))
+        PLAY_TEXT = get_font(30).render("Solitaire", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(60, 25))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
 
-        PLAY_BACK = Button(image=None, pos=(400, 460),
-                           text_input="BACK", font=get_font(50), base_color="White", hovering_color="Green")
+        PLAY_BACK = Button(image=None, pos=(1220, 25),
+                           text_input="Menu", font=get_font(25), base_color="White", hovering_color="#e3aacc")
 
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(SCREEN)
@@ -52,7 +61,7 @@ def options():
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
         OPTIONS_BACK = Button(image=None, pos=(400, 460),
-                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="e3aacc")
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(SCREEN)
@@ -74,15 +83,15 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(150).render("Solitaire", True, "#b68f40")
+        MENU_TEXT = get_font(150).render("Solitaire", True, "#870955")
         MENU_RECT = MENU_TEXT.get_rect(center=(360, 100))
 
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Button.png"), pos=(300, 250),
-                             text_input="PLAY", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
+                             text_input="PLAY", font=get_font(50), base_color="#e3aacc", hovering_color="White")
         OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Button.png"), pos=(300, 400),
-                                text_input="OPTIONS", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
+                                text_input="OPTIONS", font=get_font(50), base_color="#e3aacc", hovering_color="White")
         QUIT_BUTTON = Button(image=pygame.image.load("assets/Button.png"), pos=(300, 550),
-                             text_input="QUIT", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
+                             text_input="QUIT", font=get_font(50), base_color="#e3aacc", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
